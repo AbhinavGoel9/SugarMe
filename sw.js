fa85bca80140c3f2a10aec4f756f8651f35dfb68
Abhinav Goel <153209537+AbhinavGoel9@users.noreply.github.com>
2026-05-09 13:15:32 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE = 'sugarme-v1';
+const ASSETS = ['/index.html', '/pages/characters.html', '/pages/dashboard.html'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
